8dfdef660f6c6325866078888c86df3c536c5df1
gshay93 <official.gshay@gmail.com>
2016-07-03 01:11:52 +0530

    input some images for icons

--- UMS/UMS/start.xaml.cs
+++ UMS/UMS/start.xaml.cs
@@ -10,30 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace UMS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for start.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class start : Window
     {
-        public MainWindow()
+        public start()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-        private void button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
 
-        }
     }
 }
